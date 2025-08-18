f6056460628c1030523750a158aeb2a094cdf118
EC2 Default User <ec2-user@ip-10-0-0-72.eu-west-1.compute.internal>
2025-08-18 07:05:43 +0000

    updated lambda

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda - via codebuild - this is now working - via API GW! - test change- and again!!')
+        'body': json.dumps('Hello from Lambda - via codebuild')
     }
